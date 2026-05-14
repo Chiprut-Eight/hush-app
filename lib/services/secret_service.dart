fcd32df783a9624395a3dc5c4817f9bb96d0b028
Chiprut <chiprut20@gmail.com>
2026-05-14 04:47:41 +0300

    fix: use correct Dart 3 null-aware value syntax in map literals

--- lib/services/secret_service.dart
+++ lib/services/secret_service.dart
@@ -117,8 +117,8 @@ class SecretService {
       final callable = FirebaseFunctions.instance.httpsCallable('revealSecret');
       final result = await callable.call({
         'secretId': secretId,
-        if (lat != null) 'userLat': lat,
-        if (lng != null) 'userLng': lng,
+        'userLat': ?lat,
+        'userLng': ?lng,
       });
       return Map<String, dynamic>.from(result.data);
     } catch (e) {
@@ -148,8 +148,8 @@ class SecretService {
       'lat': lat,
       'lng': lng,
       'isGroup': isGroup,
-      if (requiredUsers != null) 'requiredUsers': requiredUsers,
-      if (timeWindowMinutes != null) 'timeWindowMinutes': timeWindowMinutes,
+      'requiredUsers': ?requiredUsers,
+      'timeWindowMinutes': ?timeWindowMinutes,
     });
     final data = Map<String, dynamic>.from(result.data);
     if (data['success'] != true) throw Exception(data['message'] ?? 'Create failed');
@@ -175,8 +175,8 @@ class SecretService {
       'lat': lat,
       'lng': lng,
       'isGroup': isGroup,
-      if (requiredUsers != null) 'requiredUsers': requiredUsers,
-      if (timeWindowMinutes != null) 'timeWindowMinutes': timeWindowMinutes,
+      'requiredUsers': ?requiredUsers,
+      'timeWindowMinutes': ?timeWindowMinutes,
     });
     final data = Map<String, dynamic>.from(result.data);
     if (data['success'] != true) throw Exception(data['message'] ?? 'Create failed');
